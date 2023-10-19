b48d63cd48e046388b9d624545df6ebde274e470
Felipe Marques <felipe.comarques1208@gmail.com>
2023-10-19 11:08:50 -0300

    feat: Add about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,17 @@ import waifu from '../assets/waifu.jpg'
 
 const About = () => {
     return (
-        <section className="bg-violet-700/5">About</section>
+        <section className="bg-violet-700/5 py-20" id="about">
+            <div className="align-element grid md:grid-cols-2 items-center gap-16">
+                <img src={waifu} className="mx-auto m-0 h-72 rounded-full border-2 border-violet-400/50" />
+                <article>
+                    <SectionTitle text="About me" />
+                    <p className="text-violet-100 mt-8 leading-loose">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam perferendis, placeat voluptate, assumenda ratione dolores distinctio quam corrupti quas pariatur, repudiandae aliquam recusandae consectetur consequatur! Illum ad labore beatae numquam?
+                    </p>
+                </article>
+            </div>
+        </section>
     )
 }
 export default About
