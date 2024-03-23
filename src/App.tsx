1846022dccf58e06b90f1c2d49dd51b3f0c9d72c
Felipe Marques <felipe.comarques1208@gmail.com>
2024-03-23 00:17:33 -0300

    feat: Add Waves

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import './styles/waves.css'
 
 export default function App() {
   return (
     <>
       <Navbar />
-      {/* <div className="h-16"></div> */}
       <Hero />
+      <div className="spacer wave1" />
       <Skills />
+      <div className="spacer wave2" />
       <About />
+      <div className="spacer wave3" />
       <Projects />
     </>
 
