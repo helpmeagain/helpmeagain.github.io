4f14fe4a2eb74dd1881e62636cab61bfe120a335
Felipe Marques <felipe.comarques1208@gmail.com>
2024-03-24 19:16:31 -0300

    feat: Add gradient

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,18 @@
 import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
-// import waifu from '../assets/waifu.jpg'
 
 const Hero = () => {
     return (
         <>
-            <section className="bg-violet-700/5 h-screen py-60" id="home">
+            <section className="bg-gradient-to-t from-violet-700/5 via-violet-700/25 to-violet-700/5 h-screen" id="home">
                 <div className="flex items-center justify-start max-[900px]:justify-center ml-7 select-none">
-                    <article>
-                        <h1 className="text-5xl min-[600px]:text-6xl font-semibold text-violet-100">
+                    <article className='h-screen flex flex-col justify-center'>
+                        <h1 className="text-5xl min-[600px]:text-6xl font-semibold text-violet-200">
                             Welcome, I'm <br className='min-[900px]:hidden' />Felipe Marques!
                         </h1>
 
                         <img src="https://readme-typing-svg.herokuapp.com?font=Fira+Mono&size=25&duration=3000&pause=5000&color=EDE9FE&random=false&width=435&lines=Backend+dev+%26+game+dev"
                             alt="Typing SVG"
-                            className='mt-4'
+                            className='mt-4 w-7/12'
                         />
 
                         <div className="flex gap-x-3 ">
