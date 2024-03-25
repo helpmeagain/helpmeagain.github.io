152edbc8545331d6ec48ed4e8326fe0544bae6e8
Felipe Marques <felipe.comarques1208@gmail.com>
2024-03-25 00:52:58 -0300

    feat: Add translation functionality

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,16 @@
 import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+import { useTranslation } from 'react-i18next';
 
 const Hero = () => {
+    const { t } = useTranslation();
+
     return (
         <>
             <section className="bg-gradient-to-t from-violet-700/5 via-violet-700/25 to-violet-700/5 h-screen" id="home">
-                <div className="flex items-center justify-start max-[900px]:justify-center ml-7 select-none">
+                <div className="flex items-center justify-start max-[900px]:justify-center ml-6 select-none">
                     <article className='h-screen flex flex-col justify-center'>
-                        <h1 className="text-5xl min-[600px]:text-6xl font-semibold text-violet-200">
-                            Welcome, I'm <br className='min-[900px]:hidden' />Felipe Marques!
+                        <h1 className="text-4xl min-[600px]:text-6xl font-semibold text-violet-200">
+                            {t('hero.welcome')} <br className='min-[900px]:hidden' />Felipe Marques!
                         </h1>
 
                         <img src="https://readme-typing-svg.herokuapp.com?font=Fira+Mono&size=25&duration=3000&pause=5000&color=EDE9FE&random=false&width=435&lines=Backend+dev+%26+game+dev"
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,17 @@ import { useState } from 'react';
 import { links } from '../data';
 import { FiMenu, FiX } from "react-icons/fi";
 import { MdTranslate } from "react-icons/md";
+import { useTranslation } from 'react-i18next';
 import "../styles/navbar.css"
 
 const Navbar = () => {
+    const { i18n } = useTranslation();
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const changeLanguage = () => {
+        const language = i18n.language === 'en' ? 'pt' : 'en';
+        i18n.changeLanguage(language);
+    };
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -18,7 +25,7 @@ const Navbar = () => {
     return (
         <nav className="navbar-style navbar-settings">
             <div className='flex'>
-                <h2 className='fira-mono-bold flex items-center text-2xl font-mono font-bold whitespace-nowrap text-violet-100'>
+                <h2 className='fira-mono-bold flex items-center text-2xl max-sm:text-lg font-mono font-bold whitespace-nowrap text-violet-100'>
                     &lt;<span className="text-violet-500">Felipe Marques</span>/&gt;
                 </h2>
                 <ul className={isMenuOpen ? "navbar-list active" : "navbar-list"}>
@@ -33,7 +40,7 @@ const Navbar = () => {
             </div>
             <div className='flex gap-2'>
                 <div className="flex items-center justify-center">
-                    <button className="navbar-button ml-2 cursor-not-allowed">
+                    <button className="navbar-button ml-2" onClick={changeLanguage}>
                         <MdTranslate className="md:ml-1" />
                     </button>
                 </div>
